140f67bd5354331338fb23866995354cbcf92c94
emiliosetiadarma <emiliosetiadarma@gmail.com>
2023-11-25 21:16:18 -0800

    finish tools

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,6 +4,7 @@ from langchain import PromptTemplate
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 
 information = """
@@ -19,20 +20,22 @@ Musk has expressed views that have made him a polarizing figure. He has been cri
 if __name__ == "__main__":
     print("Hello LangChain!")
 
+    linkedin_profile_url = linkedin_lookup_agent(name="Eden Marco")
+
     summary_template = """
         given the information {information} about a person from I want you to create:
         1. a short summary
         2. two interesting facts about them
     """
 
-    summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
+    summary_prompt_template = PromptTemplate(
+        input_variables=["information"], template=summary_template
+    )
 
     # temperature will decide how creative the language model will be
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = LLMChain(llm=llm, prompt=summary_prompt_template)
 
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/harrison-chase-961287118/"
-    )
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
     print(chain.run(information=linkedin_data))
